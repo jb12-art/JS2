da0a3785ba3d6e490597ac6ffd4cb773a0c974c9
jb12-art <jbqw1@msn.com>
2025-10-17 14:47:37 +0300

    API

--- js/index.js
+++ js/index.js
@@ -1,1 +1,86 @@
+// API details
 export const API_BASE = "https://v2.api.noroff.dev";
+export const API_AUTH = "/auth";
+export const API_REGISTER = "/register";
+export const API_LOGIN = "/login";
+
+// Local storage functions
+export function save(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function load(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+// GET post function / API calls
+export async function getPosts() {
+  const response = await fetch(API_BASE + "/social/posts", {
+    headers: {
+      Authorization: `Bearer ${load("token")}`,
+    },
+  });
+  return await response.json();
+}
+
+// Register function / API calls
+export async function register(name, email, password) {
+  const response = await fetch(API_BASE + API_AUTH + API_REGISTER, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ name, email, password }),
+  });
+
+  if (response.ok) {
+    return await response.json();
+  }
+
+  throw new Error("Could not register the account");
+}
+
+// Login function / API calls
+export async function login(email, password) {
+  const response = await fetch(API_BASE + API_AUTH + API_LOGIN, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (response.ok) {
+    const { accessToken, ...profile } = (await response.json()).data;
+    save("token", accessToken);
+    save("profile", profile);
+    return profile;
+  }
+
+  throw new Error("Could not login the account");
+}
+
+// Authentication/ event listener
+export async function onAuth(event) {
+  event.preventDefault();
+  const name = event.target.name.value;
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+
+  if (event.submitter.dataset.auth === "login") {
+    await login(email, password);
+  } else {
+    await register(name, email, password);
+    await login(email, password);
+  }
+
+  const posts = await getPosts();
+  console.log(posts);
+}
+
+// Attach Auth event listener
+export function setAuthListener() {
+  document.forms.auth.addEventlistener("submit", onAuth);
+}
+
+setAuthListener();
